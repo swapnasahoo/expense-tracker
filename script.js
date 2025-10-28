5c12cb007ba2d20bb0bcc90acc5c5e2eb4dd03f8
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-10-28 23:21:13 +0530

    Added functionality required to create transaction

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const transactionList = [];
+
 document.querySelector('.js-open-sidebar').addEventListener('click', () => {
   document.querySelector('.sidebar').classList.toggle('active');
   document.querySelector('nav ul li:first-child').classList.toggle('active');
@@ -13,4 +15,41 @@ document.body.addEventListener('keydown', (e) => {
     document.querySelector('.sidebar').classList.toggle('active');
     document.querySelector('nav ul li:first-child').classList.toggle('active');
   }
+});
+
+function createTransaction() {
+  const transAmount = document.querySelector('.trans-amount-input');
+  const transType = document.querySelector('.trans-type-input');
+
+  transactionList.push({
+    amount: transAmount.value,
+    date: '28th Sep 2025 22:41',
+    type: transType.value,
+  });
+
+  renderTransaction();
+}
+
+function renderTransaction() {
+  let html = '';
+
+  transactionList.forEach((transaction) => {
+    let color =
+      transaction.type === 'income'
+        ? 'var(--color-success)'
+        : 'var(--color-danger)';
+    let transSign = transaction.type === 'income' ? '+' : '-';
+
+    html += `
+        <div class="transaction">
+          <p class="trans-amount" style="color: ${color}">${transSign}${transaction.amount}</p>
+          <p>${transaction.date}</p>
+        </div>`;
+
+    document.querySelector('.transaction-list').innerHTML = html;
+  });
+}
+
+document.querySelector('.trans-add-btn').addEventListener('click', () => {
+  createTransaction();
 });
