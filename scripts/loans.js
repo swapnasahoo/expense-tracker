d60ca776102989b44f8ff23ade93529074c0f79f
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-11-02 05:16:41 +0530

    Fixed extra loans getting added after creating a new loan

--- scripts/loans.js
+++ scripts/loans.js
@@ -6,38 +6,38 @@ const loanCreateBtn = document.querySelector('.show-loan-create');
 
 loanCreateBtn.addEventListener('click', () => {
   popUp.innerHTML = `
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="1.75rem"
-            viewBox="0 -960 960 960"
-            width="1.75rem"
-            fill="#ffffff"
-            class="close-popup-icon"
-          >
-            <path
-              d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"
-            />
-          </svg>
-          <div class="create-trans">
-            <h2>Add Loan</h2>
-            <input
-              type="text"
-              placeholder="Enter the loan name"
-              class="loan-name-input"
-              required
-            />
-            <input
-              type="number"
-              placeholder="Enter the loan amount"
-              class="loan-amount-input"
-              required
-            />
-          </div>
-          <div class="trans-type-div">
-            <button type="submit" class="loan-add-btn">Add</button>
-          </div>
-        </div>`;
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="1.75rem"
+              viewBox="0 -960 960 960"
+              width="1.75rem"
+              fill="#ffffff"
+              class="close-popup-icon"
+            >
+              <path
+                d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"
+              />
+            </svg>
+            <div class="create-trans">
+              <h2>Add Loan</h2>
+              <input
+                type="text"
+                placeholder="Enter the loan name"
+                class="loan-name-input"
+                required
+              />
+              <input
+                type="number"
+                placeholder="Enter the loan amount"
+                class="loan-amount-input"
+                required
+              />
+            </div>
+            <div class="trans-type-div">
+              <button type="submit" class="loan-add-btn">Add</button>
+            </div>
+          </div>`;
 
   document.querySelector('.close-popup-icon').addEventListener('click', () => {
     closePopUp();
@@ -69,14 +69,13 @@ function renderLoanList() {
   let html = '';
 
   loanList.forEach((loan) => {
-    html = `
+    html += `
         <div class="loan">
           <img src="/icons/bank-icon.svg" class="loan-icon" />
           <p class="loan-name">${loan.name}</p>
           <p>${loan.amount}</p>
         </div>`;
-    document.querySelector('.loan-list').innerHTML += html;
   });
-
+  document.querySelector('.loan-list').innerHTML = html;
   localStorage.setItem('loanList', JSON.stringify(loanList));
 }
