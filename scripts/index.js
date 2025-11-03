435b81a486bf06aaab693253ff46dafe93488061
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-11-03 14:29:08 +0530

    Improved code for making monthName

--- scripts/index.js
+++ scripts/index.js
@@ -44,45 +44,11 @@ function createTransaction() {
       dateOrdinal = 'th';
   }
 
-  // SWITCH CASE FOR GENERATING monthName('Jan', 'Feb', etc..)
-  switch (month) {
-    case 1:
-      monthName = 'Jan';
-      break;
-    case 2:
-      monthName = 'Feb';
-      break;
-    case 3:
-      monthName = 'Mar';
-      break;
-    case 4:
-      monthName = 'Apr';
-      break;
-    case 5:
-      monthName = 'May';
-      break;
-    case 6:
-      monthName = 'Jun';
-      break;
-    case 7:
-      monthName = 'Jul';
-      break;
-    case 8:
-      monthName = 'Aug';
-      break;
-    case 9:
-      monthName = 'Sep';
-      break;
-    case 10:
-      monthName = 'Oct';
-      break;
-    case 11:
-      monthName = 'Nov';
-      break;
-    case 12:
-      monthName = 'Dec';
-      break;
-  }
+  // FOR GENERATING monthName('Jan', 'Feb', etc..)
+  // prettier-ignore
+  const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+  monthName = monthNames[month];
 
   transactionList.push({
     amount: transAmount.value,
