4c9b534cf7e5989f031e8fdcef700a11de12b967
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-11-03 22:48:54 +0530

    Added link between index.html with transactions.html and loans.html

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,11 @@ document.querySelector('.hamburger-icon').addEventListener('click', () => {
 document.querySelector('.close-icon').addEventListener('click', () => {
   document.querySelector('.hamburger-menu').classList.remove('active');
 });
+
+document.querySelector('.cta-trans').addEventListener('click', () => {
+  window.location.href = 'transactions.html';
+});
+
+document.querySelector('.cta-loan').addEventListener('click', () => {
+  window.location.href = 'loans.html';
+});
