a5a0ef1a39fef902a3bf5adc882cfb40910b183d
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-11-05 20:40:05 +0530

    Added pop up for profile menu and made it functional

--- scripts/index.js
+++ scripts/index.js
@@ -13,3 +13,33 @@ document.querySelector('.cta-trans').addEventListener('click', () => {
 document.querySelector('.cta-loan').addEventListener('click', () => {
   window.location.href = 'loans.html';
 });
+
+// OPENING PROFILE MENU
+function openProfileMenu() {
+  document.querySelector('.profile-menu').classList.remove('close');
+  document.querySelector('.profile-menu').classList.add('active');
+  document.querySelector('.overlay').classList.add('active');
+  document.body.classList.add('active');
+}
+
+document.querySelector('.avatar-circle').addEventListener('click', () => {
+  openProfileMenu();
+});
+
+// CLOSING PROFILE MENU
+function closeProfileMenu() {
+  document.querySelector('.profile-menu').classList.remove('active');
+  document.querySelector('.profile-menu').classList.add('close');
+  document.querySelector('.overlay').classList.remove('active');
+  document.body.classList.remove('active');
+}
+
+document.querySelector('.profile-close-icon').addEventListener('click', () => {
+  closeProfileMenu();
+});
+
+document.body.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    closeProfileMenu();
+  }
+});
