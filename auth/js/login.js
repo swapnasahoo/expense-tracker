ab815218dc55f6389946ddd1335af40c2950b33e
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-11-06 19:23:43 +0530

    Added feature to validate the form before submission and handle errors and display to user

--- auth/js/login.js
+++ auth/js/login.js
@@ -2,8 +2,21 @@ import { auth } from './auth.js';
 import { signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js';
 
 // INPUT FIELDS FOR LOGGING INTO ACCOUNT(LOG IN)
+const inputs = document.querySelectorAll('input');
 const emailLogin = document.querySelector('.email-login-input');
 const passwordLogin = document.querySelector('.password-login-input');
+
+// FUNCTION TO VALIDATE LOGIN FORM
+function validateForm() {
+  for (const input of inputs) {
+    if (!input.checkValidity()) {
+      input.reportValidity();
+      return;
+    }
+  }
+
+  login();
+}
 
 // FUNCTION TO LOGIN(SIGN IN)
 async function login() {
@@ -16,10 +29,13 @@ async function login() {
 
     window.location.href = '../index.html';
   } catch (e) {
-    console.log(`Error: ${e}`);
+    console.log(`Error: ${e.code}`);
+    if (e.code === 'auth/invalid-credential') {
+      document.querySelector('.error-msg').innerHTML = 'Invalid credentials';
+    }
   }
 }
 
 document.querySelector('.login-btn').addEventListener('click', () => {
-  login();
+  validateForm();
 });
